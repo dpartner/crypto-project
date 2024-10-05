abf77a54e21e6c6130d4589df4eb2f1f46ac2f06
dpartner <d.partner.ua@gmail.com>
2024-10-05 04:39:09 +0300

    sorted history

--- src/js/balance.js
+++ src/js/balance.js
@@ -1,12 +1,21 @@
-import { fetchCoins, fetchUser } from './fakeApi.js';
+import { fetchCoins, fetchUser, fetchHistory } from './fakeApi.js';
 
 const domElements = {
   coinsList: document.querySelector('.tokens-list'),
   eye: document.querySelector('.balance-button-eye'),
   balance: document.querySelector('.balance'),
   tokensContainer: document.querySelector('.tokens-list-wrap'),
+  historyButton: document.querySelector('.tokens-history-button'),
+  balanceSection: document.querySelector('.balance-section'),
+  tokensSection: document.querySelector('.tokens-section'),
+  historySection: document.querySelector('.history-section'),
+  closeHistoryButton: document.querySelector('.close-history'),
+  historyListWrap: document.querySelector('.history-list-wrap'),
 };
 
+// ------------Add Markup-------------------
+
+addMarkup();
 const user = fetchUser();
 
 async function addMarkup() {
@@ -15,10 +24,11 @@ async function addMarkup() {
   domElements.balance.innerHTML = (await user).balance;
 }
 
-addMarkup();
+// ------------Hide balance-------------------
 
 domElements.eye.addEventListener('click', handleHiddenBalance);
 
+// Hiiden user balance (eye button)
 async function handleHiddenBalance(e) {
   e.preventDefault();
   if (domElements.eye.classList.contains('hidden')) {
@@ -30,6 +40,7 @@ async function handleHiddenBalance(e) {
   domElements.balance.innerHTML = '😎😎😎';
 }
 
+//------------- Create tokens wrap markup----------------
 async function createMarkup() {
   // Передаешь вызову функции api нужный аргумент например UserId
   const data = await fetchCoins();
@@ -48,7 +59,7 @@ async function createMarkup() {
     return `
               <li class="tokens-list-item">
                 <div>
-                  <img src="../img/${coin.symbolName}" width="32" height="32" alt="tonkoin">
+                  <img src="../img/${coin.symbolName}" width="32" height="32" alt="${coin.name}">
                 </div>
                 <div class="tokens-list-item-values-wrap">
                   <div class="tokens-item-heading">
@@ -67,6 +78,7 @@ async function createMarkup() {
   return markup;
 }
 
+// Animation tokens wrap
 function scroolTokensWrap() {
   setTimeout(() => {
     domElements.tokensContainer.scroll({
@@ -81,7 +93,160 @@ function scroolTokensWrap() {
     });
   }, 2500);
 }
-
+// Start Animation tokens wrap when window loaded
 window.addEventListener('load', () => {
   scroolTokensWrap();
 });
+
+// ------------History-------------------
+
+domElements.historyButton.addEventListener('click', handleShowHistory);
+domElements.closeHistoryButton.addEventListener('click', handleCloseHistory);
+
+async function handleShowHistory(e) {
+  e.preventDefault();
+  // LOADER
+  const markup = await createhistoryMarkup();
+  domElements.historyListWrap.innerHTML = markup;
+
+  toggleHistory();
+}
+
+function handleCloseHistory(e) {
+  e.preventDefault();
+  // LOADER
+  toggleHistory();
+}
+
+function toggleHistory() {
+  domElements.historySection.classList.toggle('hidden');
+  domElements.tokensSection.classList.toggle('hidden');
+  domElements.balanceSection.classList.toggle('hidden');
+}
+
+// Grouping array history transactions from date
+async function dataGroupingHistory() {
+  const history = await fetchHistory();
+  console.log(history);
+  const historyGroups = [];
+  for (const trans of history) {
+    const dateTrans = new Date(trans.date);
+    const dateTransWithoutTime = getDateWithoutTime(dateTrans);
+    if (historyGroups.length) {
+      console.log(historyGroups);
+      const findedGroup = historyGroups.find(group => {
+        console.log(1);
+        const dateGroup = new Date(group.date);
+        return (
+          dateTrans.getDate() === dateGroup.getDate() &&
+          dateTrans.getMonth() === dateGroup.getMonth() &&
+          dateTrans.getFullYear() === dateGroup.getFullYear()
+        );
+      });
+      if (findedGroup) {
+        console.log(2);
+        findedGroup.transactions.push(trans);
+      } else {
+        historyGroups.push({
+          date: dateTransWithoutTime,
+          transactions: [trans],
+        });
+      }
+    } else {
+      console.log(3);
+      historyGroups.push({
+        date: dateTransWithoutTime,
+        transactions: [trans],
+      });
+    }
+  }
+
+  // for (const transaction of history) {
+  //   const dateTrans = new Date(transaction.date);
+  //   if (historyGroup.length) {
+  //     for (const group of historyGroup) {
+  //       const dateGroup = new Date(group.date);
+  //       if (
+  //         dateTrans.getDate() === dateGroup.getDate() &&
+  //         dateTrans.getMonth() === dateGroup.getMonth() &&
+  //         dateTrans.getFullYear() === dateGroup.getFullYear()
+  //       ) {
+  //         console.log(`1 ${dateTrans.getDate()} ${dateGroup.getDate()}`);
+  //         group.transactions.push(transaction);
+  //         break;
+  //       } else {
+  //         console.log(`2 ${dateTrans.getDate()} ${dateGroup.getDate()}`);
+
+  //         historyGroup.push({
+  //           date: transaction.date,
+  //           transactions: [transaction],
+  //         });
+  //         break;
+  //       }
+  //     }
+  //   } else {
+  //     console.log(`3 ${dateTrans}`);
+  //     historyGroup.push({
+  //       date: transaction.date,
+  //       transactions: [transaction],
+  //     });
+  //   }
+  // }
+
+  return historyGroups;
+}
+
+function getDateWithoutTime(date) {
+  // Create a new date object to avoid modifying the original date
+  const dateWithoutTime = new Date(date);
+
+  // Set seconds and milliseconds to zero
+  dateWithoutTime.setSeconds(0, 0);
+
+  // Format date components with padding
+  const year = dateWithoutTime.getFullYear();
+  const month = String(dateWithoutTime.getMonth() + 1).padStart(2, '0');
+  const day = String(dateWithoutTime.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
+async function createhistoryMarkup() {
+  const groups = await dataGroupingHistory();
+  const groupsSorted = groups.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date);
+  });
+  console.log(groups);
+  const markup = groupsSorted.map(group => {
+    const sortedTransactions = group.transactions.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+    const date = new Date(group.date);
+    const day = date.getDate();
+    const month = '0' + (date.getMonth() + 1);
+    const year = date.getFullYear().toString().slice(2);
+    return `<h2 class="history-list-date">${day}.${month}.${year}</h2>
+    <ul class="history-list">${sortedTransactions
+      .map(trans => {
+        let valueClass = 'buy';
+        let valueSymbol = '+';
+        if (trans.operation === 'sold') {
+          valueClass = 'sold';
+          valueSymbol = '-';
+        }
+        return `<li class="history-list-item">
+                <div>
+                  <img src="../img/${trans.symbolName}" width="32" height="32" alt="tonkoin">
+                </div>
+                <div class="history-item-heading">
+                  <h4>${trans.coinName}</h4>
+                  <span class="history-item-value ${valueClass}">${valueSymbol}${trans.amount}</span>
+                  <span class="history-item-code ${valueClass}">${trans.coinCode}</span>
+                </div>
+              </li>`;
+      })
+      .join('')} </ul>
+    `;
+  });
+  return markup.join('');
+}
--- src/js/fakeApi.js
+++ src/js/fakeApi.js
@@ -9,6 +9,11 @@ export async function fetchUser() {
   const data = user;
   return data;
 }
+export async function fetchHistory() {
+  // data = await fetch('api'); Запрос на твой api  за данными
+  const data = history;
+  return data;
+}
 
 const coins = [
   {
@@ -74,3 +79,87 @@ const user = {
   balance: '23567',
   photoUrl: '../img/user-photo.png',
 };
+
+const history = [
+  {
+    id: 1,
+    date: 'Mar 12 2024 10:00:00 AM',
+    coinName: 'USD₮',
+    coinCode: 'USDT',
+    symbolName: 'usdt.png',
+    operation: 'buy',
+    amount: '3,53',
+  },
+  {
+    id: 2,
+    date: 'Mar 13 2024 11:00:00 AM',
+    coinName: 'DOGS',
+    coinCode: 'DOGS',
+    symbolName: 'dogs.png',
+    operation: 'sold',
+    amount: '5,27',
+  },
+  {
+    id: 3,
+    date: 'Mar 12 2024 09:00:00 AM',
+    coinName: 'Toncoin',
+    coinCode: 'TON',
+    symbolName: 'tonkoin.png',
+    operation: 'buy',
+    amount: '5,65',
+  },
+  {
+    id: 4,
+    date: 'Mar 14 2024 10:00:00 AM',
+    coinName: 'USD₮',
+    coinCode: 'USDT',
+    symbolName: 'usdt.png',
+    operation: 'buy',
+    amount: '7,35',
+  },
+  {
+    id: 5,
+    date: 'Mar 13 2024 11:00:00 AM',
+    coinName: 'DOGS',
+    coinCode: 'DOGS',
+    symbolName: 'dogs.png',
+    operation: 'buy',
+    amount: '2,03',
+  },
+  {
+    id: 6,
+    date: 'Mar 15 2024 09:00:00 AM',
+    coinName: 'Toncoin',
+    coinCode: 'TON',
+    symbolName: 'tonkoin.png',
+    operation: 'buy',
+    amount: '2,65',
+  },
+  {
+    id: 7,
+    date: 'Mar 13 2024 10:00:00 AM',
+    coinName: 'USD₮',
+    coinCode: 'USDT',
+    symbolName: 'usdt.png',
+    operation: 'sold',
+    amount: '3,65',
+  },
+  {
+    id: 8,
+    date: 'Mar 15 2024 08:00:00 AM',
+    coinName: 'DOGS',
+    coinCode: 'DOGS',
+    symbolName: 'dogs.png',
+    operation: 'sold',
+    amount: '3,25',
+  },
+  {
+    id: 9,
+    date: 'Mar 14 2024 09:00:00 AM',
+    coinName: 'Toncoin',
+    coinCode: 'TON',
+    symbolName: 'tonkoin.png',
+    operation: 'buy',
+    amount: '9,65',
+  },
+];
